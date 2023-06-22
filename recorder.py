a0d31bd3ae7874bf6f496cb98b41bed613fab0fe
Himanshu Shukla <91239771+hs0047@users.noreply.github.com>
2023-06-22 17:20:31 +0530

    initial checkin

---
+++ recorder.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+import asyncio
+import time
+import sounddevice as sd
+import soundfile as sf
+import pyautogui
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import sys
+
+# Load environment variables from .env file
+load_dotenv()
+
+FS = 44100
+BUFFER = []
+CHECK_FREQUENCY = 5  # seconds
+TEMPLATE_PATH = r'zoombot_images\meeting_ended_notification.png'
+DB_CONNECTION = os.environ.get('DB_URI')
+
+class Recorder:
+    def __init__(self, link_id):
+        self.device_name = None
+        self.device_id = None
+        self.recording = False
+        self.link_id = link_id
+        self.stream = None
+        self.client = MongoClient(DB_CONNECTION)
+        self.db = self.client["Meeting_automation"]
+        self.col_cables = self.db["serverCables"]
+        self.col_links = self.db["Zoom_meeting_link"]
+
+    def callback(self, indata, frames, time, status):
+        if self.recording:
+            BUFFER.extend(indata[:, 0])  # Assuming mono recording
+
+    async def start_recording(self):
+        print(f"Starting recording")
+
+        document = self.col_cables.find_one({"name": "Server 1"})  # Get document by server name
+        if document:
+            if len(document['availableCables']) == 0:
+                print("No cables available for recording, try again later.")
+                raise Exception("No cables available for recording")
+            self.device_name = document['availableCables'].pop(0)
+            document['busyCables'].append(self.device_name)
+            self.col_cables.update_one({"_id": document["_id"]}, {"$set": {"availableCables": document['availableCables'], "busyCables": document['busyCables']}})
+            # Update Meeting_link collection
+            self.col_links.update_one({"_id": self.link_id}, {"$set": {"status": "recording"}})
+            print(f'Recording on {self.device_name}') 
+
+        device_info = sd.query_devices()
+        for i, device in enumerate(device_info):
+            if device['name'] == self.device_name:
+                self.device_id = i
+                break
+
+        if self.device_id is None:
+            raise ValueError(f"No device found with name: {self.device_name}")
+
+        self.stream = sd.InputStream(samplerate=FS, channels=1, device=self.device_id, callback=self.callback)
+        self.recording = True
+        self.stream.start()
+
+    async def stop_recording(self):
+        print("Stopping recording")
+        self.stream.stop()
+        self.recording = False
+        self.save_recording()
+
+        document = self.col_cables.find_one({"name": "Server 1"})  # Get document by server name
+        if document:
+            if self.device_name in document['busyCables']:
+                document['busyCables'].remove(self.device_name)
+                document['availableCables'].insert(0, self.device_name)
+                self.col_cables.update_one({"_id": document["_id"]}, {"$set": {"availableCables": document['availableCables'], "busyCables": document['busyCables']}})
+
+        # Update Meeting_link collection
+        self.col_links.update_one({"_id": self.link_id}, {"$set": {"status": "Recording Completed", "recordedFile": "recording.wav"}})
+
+    def save_recording(self):
+        sf.write('recording.wav', np.array(BUFFER), FS)
+
+    async def check_screen(self):
+        template = cv2.imread(TEMPLATE_PATH, cv2.IMREAD_GRAYSCALE)
+        while True:
+            await asyncio.sleep(CHECK_FREQUENCY)
+            if not self.recording:
+                return
+            screenshot = pyautogui.screenshot()
+            screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+            screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+            res = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.8
+            loc = np.where(res >= threshold)
+            if len(loc[0]) > 0:
+                await self.stop_recording()
+
+async def main():
+    link_id = sys.argv[0]  
+    recorder = Recorder(link_id)  
+    await recorder.start_recording()
+    asyncio.create_task(recorder.check_screen())
+    await asyncio.sleep(25)
+    if recorder.recording:
+        await recorder.stop_recording()
+
+asyncio.run(main())
---
+++ zoombot.py
@@ -0,0 +1,268 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import cv2
+import numpy as np
+import pyautogui
+import time
+from time import sleep
+import sys
+
+# Fetch the meeting link from the command line arguments
+if len(sys.argv) > 1:
+    meeting_link = sys.argv[1]
+else:
+    print("No Zoom link provided. Exiting.")
+    sys.exit(1)
+
+# Open the browser
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--use-fake-ui-for-media-stream")  # Disable popup for allowing webcam and microphone
+chrome_options.add_argument("--disable-notifications")  # Disable notifications
+chrome_options.add_argument("--start-maximized")  # Open browser in maximized mode
+
+chrome_options.add_experimental_option('prefs', {
+  "protocol_handler": {
+    "excluded_schemes": {
+      "zoommtg": False
+    }
+  }
+})
+
+
+# Add Chrome debugging option
+# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Navigate to the meeting
+driver.get(meeting_link)
+
+time.sleep(6)  # wait for the page to load
+
+# This function will find the image on the screen and return its position
+def locate_on_screen(image_path, confidence=0.8):
+    # capture the screen
+    screenshot = pyautogui.screenshot()
+    screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+    # read the image file
+    template = cv2.imread(image_path, 0)
+    
+    # convert the screenshot to grayscale
+    gray_screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+
+    # match the template with the screenshot
+    match = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
+
+    # if the match is not good enough return None
+    if max_val < confidence:
+        return None
+
+    return max_loc
+
+# This function will click on the given position of an image on the screen
+def click_on_image(image_path, position='center'):
+    loc = locate_on_screen(image_path)
+
+    if loc is not None:
+        # get the size of the image
+        img = cv2.imread(image_path)
+        h, w, _ = img.shape
+
+        if position == 'top-right':
+            # calculate the top right corner of the image
+            # click_position = (loc[0] + w-1, loc[1])
+            click_position = (loc[0] + w - 11, loc[1] + 10)
+        elif position == 'bottom-center':
+            # calculate the bottom center of the image
+            click_position = (loc[0] + w // 2, loc[1] + h-10)
+        elif position == 'right-center':
+        # calculate the right-center of the image
+            click_position = (loc[0] + w, loc[1] + h // 2)
+        else:
+            # calculate the center of the image
+            click_position = (loc[0] + w // 2, loc[1] + h // 2)
+
+        # move the cursor to the click_pos of the image and click
+        pyautogui.moveTo(click_position)
+        pyautogui.click()
+        return True
+    else:
+        return False
+
+
+# The main function to join a meeting
+def join_meeting(meeting_link): 
+
+    click_on_image(r'zoombot_images\allow_button.png')
+    click_on_image(r'zoombot_images\allow_button1.png')
+    click_on_image(r'zoombot_images\cookies_button.png', 'top-right')
+    click_on_image(r'zoombot_images\cross_button.png','top-right')
+    
+    time.sleep(2)
+    click_on_image(r'zoombot_images\exit_login_button.png', 'top-right')
+    
+    time.sleep(2)
+
+    click_on_image(r'zoombot_images\launch_meeting_button.png')
+    
+    time.sleep(5)
+
+    click_on_image(r'zoombot_images\cross_button.png','top-right')
+    click_on_image(r'zoombot_images\exit_login_button.png', 'top-right')
+    time.sleep(2)
+
+    click_on_image(r'zoombot_images\browser_button.png')
+
+    # _____________
+    
+    # Define a start time
+    start_time = time.time()
+
+    # Loop until the audio button appears or 50 seconds have passed
+    while not click_on_image(r'zoombot_images\computer_audio_button.png') and time.time() - start_time < 120:
+        # wait for a bit before trying again
+        sleep(5)
+
+    # If the computer audio button did not appear within 50 seconds, perform the alternative actions
+    if time.time() - start_time >= 120:
+        click_on_image(r'zoombot_images\enter_name_button.png')
+        pyautogui.write('Bot')
+        click_on_image(r'zoombot_images\join_button.png')
+        time.sleep(10)
+        click_on_image(r'zoombot_images\video_button1.png')
+        
+        time.sleep(2)
+        
+        # Click on the microphone button to turn it off
+        if not click_on_image(r'zoombot_images\mute_button1.png'):
+            print("Couldn't find the mute button 1")
+            return
+
+        # Rest of the alternative actions...
+        # Moving the cursor in the centre of the screen
+        screen_width, screen_height = pyautogui.size()                 # Get the screen size
+        center_x, center_y = screen_width // 2, screen_height // 2     # Calculate the center of the screen
+        pyautogui.moveTo(center_x, center_y)                           # Move the mouse to the center of the screen
+
+        # Move the cursor for n seconds
+        start_time = time.time()
+
+        # Loop for 2 seconds
+        while time.time() - start_time < 2:
+            # Move the mouse cursor by 100 pixels in x and y direction
+            pyautogui.move(100, 100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+            # Move the mouse cursor back to initial position
+            pyautogui.move(-100, -100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+        # Click on the More options button
+        if not click_on_image(r'zoombot_images\more_options_button.png1', 'top-right'):
+            print("Couldn't find the more options button 1")
+            return
+        
+        time.sleep(1)
+        
+        if not click_on_image(r'zoombot_images\audio_settings_button1.png'):
+            print("Couldn't find the audio settings button 1")
+            return
+        
+        time.sleep(1)
+
+        while not click_on_image(r'zoombot_images\test_speaker_button1.png', 'right-center'):
+            # wait for a bit before trying again
+            sleep(1)
+
+        time.sleep(1)
+
+        if not click_on_image(r'zoombot_images\line_1_button1.png'):
+            print("Couldn't find the Line 1 button 1")
+            return
+        
+        time.sleep(1)
+
+        if not click_on_image(r'zoombot_images\exit_settings_button1.png','right-center'):
+            print("Couldn't find the exit settings button 1")
+            return
+
+    else:
+    # Continue with the regular actions if the computer audio button appeared within 50 seconds
+        click_on_image(r'zoombot_images\maximize_button.png')
+    
+
+        start_time = time.time()
+
+        # Loop for 2 seconds
+        while time.time() - start_time < 2:
+            # Move the mouse cursor by 100 pixels in x and y direction
+            pyautogui.move(100, 100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+            # Move the mouse cursor back to initial position
+            pyautogui.move(-100, -100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+        click_on_image(r'zoombot_images\video_button.png')
+        
+        time.sleep(2)
+        
+        # Click on the microphone button to turn it off
+        if not click_on_image(r'zoombot_images\mute_button.png'):
+            print("Couldn't find the mute button")
+            return
+
+        # Moving the cursor in the centre of the screen
+        screen_width, screen_height = pyautogui.size()                 # Get the screen size
+        center_x, center_y = screen_width // 2, screen_height // 2     # Calculate the center of the screen
+        pyautogui.moveTo(center_x, center_y)                           # Move the mouse to the center of the screen
+
+        # Move the cursor for n seconds
+        start_time = time.time()
+
+        # Loop for 2 seconds
+        while time.time() - start_time < 2:
+            # Move the mouse cursor by 100 pixels in x and y direction
+            pyautogui.move(100, 100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+            # Move the mouse cursor back to initial position
+            pyautogui.move(-100, -100, duration=0.25)
+            time.sleep(0.25)  # pause a bit before next movement
+
+        # Click on the More options button
+        if not click_on_image(r'zoombot_images\more_options_button.png', 'top-right'):
+            print("Couldn't find the more options button")
+            return
+        
+        time.sleep(1)
+        
+        if not click_on_image(r'zoombot_images\audio_settings_button.png'):
+            print("Couldn't find the audio settings button")
+            return
+        
+        time.sleep(1)
+
+        while not click_on_image(r'zoombot_images\test_speaker_button.png', 'right-center'):
+            # wait for a bit before trying again
+            sleep(1)
+
+        time.sleep(1)
+
+        if not click_on_image(r'zoombot_images\line_1_button.png'):
+            print("Couldn't find the Line 1 button")
+            return
+        
+        time.sleep(1)
+
+        if not click_on_image(r'zoombot_images\exit_settings_button.png','right-center'):
+            print("Couldn't find the exit settings button")
+            return
+    
+# Call the function with your meeting link
+join_meeting(meeting_link)
