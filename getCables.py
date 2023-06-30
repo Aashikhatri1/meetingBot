d7ceb40037dc09fa67bdaa229164c74f704aa386
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-06-30 18:22:17 +0530

    Update getCables.py

--- getCables.py
+++ getCables.py
@@ -14,25 +14,25 @@ client = MongoClient(DB_CONNECTION, tlsCAFile=ca)
 db = client["Meeting_automation"]  # replace with your database name
 collection = db["serverCables"]  # replace with your collection name
 
-
-def get_available_cable():
-    """Fetches an available cable from the MongoDB collection."""
-    document = collection.find_one({"name": "Server 1"})
-    if document and document.get("availableCables"):
-        return document["availableCables"][0]  # return first available cable
-    return None
-
-
-def make_cable_available(cable_name):
-    """Moves a cable from 'busy' to 'available' in the MongoDB collection."""
-    result = collection.update_one(
-        {"name": "Server 1"},
-        {
-            "$pull": {"busyCables": cable_name},  # remove from 'busyCables'
-            "$push": {"availableCables": cable_name},  # add to 'availableCables'
-        },
-    )
-    return result.modified_count > 0  # return whether a document was modified
+class ServerHandler:
+    def get_available_cable():
+        """Fetches an available cable from the MongoDB collection."""
+        document = collection.find_one({"name": "Server 1"})
+        if document and document.get("availableCables"):
+            return document["availableCables"][0]  # return first available cable
+        return None
+    
+    
+    def make_cable_available(cable_name):
+        """Moves a cable from 'busy' to 'available' in the MongoDB collection."""
+        result = collection.update_one(
+            {"name": "Server 1"},
+            {
+                "$pull": {"busyCables": cable_name},  # remove from 'busyCables'
+                "$push": {"availableCables": cable_name},  # add to 'availableCables'
+            },
+        )
+        return result.modified_count > 0  # return whether a document was modified
 
 
 # Test the functions
