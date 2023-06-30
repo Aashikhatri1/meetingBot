e03b62251a63684771f1d684ac91e2e3c548e4d0
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-06-30 17:12:24 +0530

    Update getCables.py

--- getCables.py
+++ getCables.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
+import os
+import certifi
+ca = certifi.where()
 
 class ServerHandler:
+    DB_CONNECTION = os.environ.get('DB_URI')
     def __init__(self, connection_string):
-        self.client = MongoClient(connection_string)
-        self.db = self.client['YourDatabaseName']
-        self.collection = self.db['YourCollectionName']
+        self.client = MongoClient(DB_CONNECTION, tlsCAFile = ca)
+        self.db = self.client['Meeting_automation']
+        self.collection = self.db['Zoom_meeting_link']
 
     def get_first_available_cable(self):
         server_data = self.collection.find_one({"_id": 1})  # assuming server id is 1
