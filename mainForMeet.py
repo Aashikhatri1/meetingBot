244433f6aee25067878a0425daf0ee5b5193959a
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-07-03 14:26:44 +0530

    Update mainForMeet.py

--- mainForMeet.py
+++ mainForMeet.py
@@ -1,89 +1,96 @@
-import subprocess
-from pymongo import MongoClient
-import time
-from dotenv import load_dotenv
-import os
-import certifi
-from getCables import ServerHandler  # import ServerHandler from getCables.py
-from zoombot import create_browser_instance, join_meeting, check_end_of_meeting  # Import join_meeting and end_meeting_notification from zoombot.py
-ca = certifi.where()
+from zoombot import create_browser_instance, join_meeting, check_end_of_meeting  
+driver = create_browser_instance()
+print('Joining the meeting...')
+join_meeting(driver, link, available_cable)
 
-# Load environment variables from .env file
-load_dotenv()
 
-def check_new_submissions():
-    DB_CONNECTION = os.environ.get('DB_URI')
-    try:
-        client = MongoClient(DB_CONNECTION,tlsCAFile=ca)
-        Meeting_automation = client['Meeting_automation']
-        Zoom_meeting_link = Meeting_automation['Zoom_meeting_link']
 
-        last_count = Zoom_meeting_link.count_documents({})
-        print(f'Initial count: {last_count}')  # print initial count
+# import subprocess
+# from pymongo import MongoClient
+# import time
+# from dotenv import load_dotenv
+# import os
+# import certifi
+# from getCables import ServerHandler  # import ServerHandler from getCables.py
+# from zoombot import create_browser_instance, join_meeting, check_end_of_meeting  # Import join_meeting and end_meeting_notification from zoombot.py
+# ca = certifi.where()
 
-        while True:
-            current_count = Zoom_meeting_link.count_documents({})
-            print(f'Current count: {current_count}')  # print current count
+# # Load environment variables from .env file
+# load_dotenv()
 
-            if current_count > last_count:
-                # fetch the latest document
-                cursor = Zoom_meeting_link.find().sort([('_id', -1)]).limit(1)
-                for doc in cursor:
-                    try:
-                        print(f'Document status: {doc["status"]}')  # Print the status regardless of its value
+# def check_new_submissions():
+#     DB_CONNECTION = os.environ.get('DB_URI')
+#     try:
+#         client = MongoClient(DB_CONNECTION,tlsCAFile=ca)
+#         Meeting_automation = client['Meeting_automation']
+#         Zoom_meeting_link = Meeting_automation['Zoom_meeting_link']
 
-                        # check if status is 'submitted'
-                        if doc['status'] == 'submitted':
-                            print(f'New document submitted: {doc}')
-                            link = doc['link']  # assuming 'link' is the field name for the meeting link
-                            print(f'Link: {link}')
+#         last_count = Zoom_meeting_link.count_documents({})
+#         print(f'Initial count: {last_count}')  # print initial count
 
-                            # Get the first available cable
-                            available_cable = ServerHandler.get_available_cable()
-                            print(f'First available cable: {available_cable}')
+#         while True:
+#             current_count = Zoom_meeting_link.count_documents({})
+#             print(f'Current count: {current_count}')  # print current count
 
-                            # Change the status of the document to 'processing'
-                            result = Zoom_meeting_link.update_one({'_id': doc['_id']}, {"$set": {"status": "processing"}})
+#             if current_count > last_count:
+#                 # fetch the latest document
+#                 cursor = Zoom_meeting_link.find().sort([('_id', -1)]).limit(1)
+#                 for doc in cursor:
+#                     try:
+#                         print(f'Document status: {doc["status"]}')  # Print the status regardless of its value
 
-                            # Print a success message if the update was successful
-                            if result.modified_count > 0:
-                                print('Document status updated successfully.')
+#                         # check if status is 'submitted'
+#                         if doc['status'] == 'submitted':
+#                             print(f'New document submitted: {doc}')
+#                             link = doc['link']  # assuming 'link' is the field name for the meeting link
+#                             print(f'Link: {link}')
+
+#                             # Get the first available cable
+#                             available_cable = ServerHandler.get_available_cable()
+#                             print(f'First available cable: {available_cable}')
+
+#                             # Change the status of the document to 'processing'
+#                             result = Zoom_meeting_link.update_one({'_id': doc['_id']}, {"$set": {"status": "processing"}})
+
+#                             # Print a success message if the update was successful
+#                             if result.modified_count > 0:
+#                                 print('Document status updated successfully.')
                                 
-                                # Run join_meeting with link as argument
-                                driver = create_browser_instance()
-                                print('Joining the meeting...')
-                                join_meeting(driver, link, available_cable)
+#                                 # Run join_meeting with link as argument
+#                                 driver = create_browser_instance()
+#                                 print('Joining the meeting...')
+#                                 join_meeting(driver, link, available_cable)
                                  
 
-                                # Run recorder.py
-                                print('Running recorder.py...')
-                                recorder_process = subprocess.run(['python', 'recorder.py', str(doc['_id'])], capture_output=True)                                
-                                if recorder_process.returncode == 0:
-                                    print('recorder.py finished successfully.')
+#                                 # Run recorder.py
+#                                 print('Running recorder.py...')
+#                                 recorder_process = subprocess.run(['python', 'recorder.py', str(doc['_id'])], capture_output=True)                                
+#                                 if recorder_process.returncode == 0:
+#                                     print('recorder.py finished successfully.')
 
-                                    # Get the path of the recorded audio from the stdout of recorder.py
-                                    recorded_file_path = recorder_process.stdout.decode().strip()
+#                                     # Get the path of the recorded audio from the stdout of recorder.py
+#                                     recorded_file_path = recorder_process.stdout.decode().strip()
 
-                                    # Insert the path of the recorded audio into MongoDB
-                                    result = Zoom_meeting_link.update_one({'_id': doc['_id']}, {"$set": {"recordedFile": recorded_file_path}})
-                                    if result.modified_count > 0:
-                                        print(f'Inserted audio path into MongoDB: {recorded_file_path}')
-                                    else:
-                                        print('Failed to insert audio path into MongoDB.')
-                                     # Call end_meeting_notification to keep the meeting open unless it finds a notification on screen that the host has ended the meeting
-                                    check_end_of_meeting()  
+#                                     # Insert the path of the recorded audio into MongoDB
+#                                     result = Zoom_meeting_link.update_one({'_id': doc['_id']}, {"$set": {"recordedFile": recorded_file_path}})
+#                                     if result.modified_count > 0:
+#                                         print(f'Inserted audio path into MongoDB: {recorded_file_path}')
+#                                     else:
+#                                         print('Failed to insert audio path into MongoDB.')
+#                                      # Call end_meeting_notification to keep the meeting open unless it finds a notification on screen that the host has ended the meeting
+#                                     check_end_of_meeting()  
                                    
-                                else:
-                                    print('recorder.py failed.')
-                            else:
-                                print('Failed to update document status.')
-                    except Exception as e:
-                        print(f'An error occurred while processing document: {e}')
-                last_count = current_count
-            # wait for a while before checking again
-            time.sleep(3)  # adjust according to requirement
-    except Exception as e:
-        print(f'An error occurred: {e}')
+#                                 else:
+#                                     print('recorder.py failed.')
+#                             else:
+#                                 print('Failed to update document status.')
+#                     except Exception as e:
+#                         print(f'An error occurred while processing document: {e}')
+#                 last_count = current_count
+#             # wait for a while before checking again
+#             time.sleep(3)  # adjust according to requirement
+#     except Exception as e:
+#         print(f'An error occurred: {e}')
 
-if __name__ == "__main__":
-    check_new_submissions()
+# if __name__ == "__main__":
+#     check_new_submissions()
