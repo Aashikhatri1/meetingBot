467b4102e930edf75f98afa90501b19b390f4f7d
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-07-03 13:05:10 +0530

    Update zoombot.py

--- zoombot.py
+++ zoombot.py
@@ -1,4 +1,4 @@
-g_from selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
