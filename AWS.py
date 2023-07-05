3dd3769dc79d40f039433bdf147ec7e466e948fa
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-07-05 09:54:43 +0300

    Update AWS.py

--- AWS.py
+++ AWS.py
@@ -1,3 +1,3 @@
 import boto3
 s3_client = boto3.client('s3')
-response = s3_client.upload_file('')
+response = s3_client.upload_file(r'C:\Users\Administrator\Documents\GitHub\meetingBot\recording.wav', 'customerexceldata' , 'recording.wav')
